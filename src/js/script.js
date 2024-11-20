25d4a07bb787b5151fde7ecaf4082403e1c9e06e
Araldi Théo <theo.araldi@edu.univ-fcomte.fr>
2024-11-20 10:51:41 +0100

    défilement de base précédent/suivant

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,17 @@
 // *** CAROUSEL ***
 // TODO
+const carousel = document.querySelector(".carousel__container");
+const nextButon = document.querySelector(".carousel__button--next");
+const prevButon = document.querySelector(".carousel__button--prev");
+
+if (carousel) {
+   prevButon.addEventListener("click", ()=>{
+    carousel.scrollBy({ left: -260, behavior: "smooth" })
+   }) 
+};
+
+if (carousel) {
+    nextButon.addEventListener("click", ()=>{
+     carousel.scrollBy({ left: +260, behavior: "smooth" })
+    }) 
+ };
